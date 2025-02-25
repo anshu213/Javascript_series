42d8283bec347befac4382885a6a47200e02354b
Anshuman Vishwakarma <123162801+anshu213@users.noreply.github.com>
2025-02-25 18:03:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+//Arithmetic Operators...........................
+
+//.............Addition............................
+
+let a = 50;
+let b = 30;
+
+let sum = a + b;
+console.log(sum);
+
+//..............Substraction.....................
+
+let sub = a - b;
+console.log(sub);
+
+//................multiplication.......................
+
+let into = a * b;
+console.log(into);
+
+//................division...........................
+
+let div = a / b;
+console.log(div);
+
+//.................Modulus.................
+
+let mod = a % b;
+console.log(mod);
+
+let revmod = b % a;
+console.log(revmod);
+
+
+//Comparison Operators........................
+//(==)operator...................
+
+const num1 = "20";
+const num2 = 20;
+let res = num1 == num2;
+console.log(res); //True
+
+const num3 = 20;
+const num4 = 30;
+let result = num3 == num4;
+console.log(result); //false
+
+
+//(===)operator......................
+
+const num5 = "20";
+const num6 = "20";
+let res1 = num5 === num6;
+console.log(res1); //True
+
+const num7 = "20";
+const num8 = "30";
+let res2 = num7 === num8;
+console.log(res2); //false
+
+//(!=)operator..............
+
+let x = "20";
+let y = true;
+let res3  = x != y;
+console.log(res3); //true
+
+let h = 20;
+let c = "20";
+let res4 = h != c;
+console.log(res4); 
+
+//(!==)operator............
+
+let q = "20";
+let e = 20;
+let res5 = q !== e;
+console.log(res5);
+
+
+//Exercise1...................................//......................//..........
+//Q1:- Take two numbers and print the addition of that number.
+let r = 2;
+let t = 5;
+
+let sum2 = r + t;
+console.log(sum2);// 7 //
+
+//Q2:- Given a four digit number , print its first digit.
+
+let d = 4567;
+let f = Math.floor(d / 1000);
+console.log(f);// 4 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
